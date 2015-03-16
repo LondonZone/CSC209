fa58963672369bac39275d3ecd7e72b82ce93201
c3schach <c3schach@9e3f5bd1-58b1-493b-a0a2-515a36056315>
2015-03-16 18:02:40 +0000

    changed functionc call in complex

--- a3/shell.c
+++ a3/shell.c
@@ -410,7 +410,7 @@ int execute_complex_command(command *c) {
         	close(pfd[1]); //close stdin fd
 
         	//execute_command(c->cmd1);
-        	execute_simple_command(c->cmd1);
+        	execute_simple_command(c->scmd);
         	// execvp(tokens[0],tokens);
         	//execvp(c->cmd1)
         	
@@ -438,7 +438,7 @@ int execute_complex_command(command *c) {
 					//} 
 	        		close(pfd[0]);// close stdout fd
 
-	        		execute_simple_command(c->cmd2);
+	        		execute_simple_command(c->scmd);
 
 	    		}
 	    	//parent process only
